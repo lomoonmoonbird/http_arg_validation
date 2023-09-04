2a92561962a5ec5b10097c81b63765ee667ecfd1
moonmoonbird <lomoonmoonbird@gmail.com>
2023-09-04 16:12:34 +0800

    HTTP请求参数验证

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # http_arg_validation
 通用http请求参数名，参数类型，默认值限制装饰器，支持必选参数和可选参数。
+
+# usage
+
+## 必选参数page，类型为int型，默认值为1
+```python
+@arg_parser(page=(int, 1))
+async def func(self, request):
+  pass
+```
+
+## 可选参数
+
+可选参数page，类型为int，无默认值
+```python
+@arg_parser(("page", int))
+async def func(self, request):
+  pass
+
+```
+
---
+++ http_req_args.py
@@ -0,0 +1,58 @@
+# --*-- coding: utf-8 --*--
+
+import functools
+
+
+from exceptions import RequestParamError
+
+def arg_parser(*required_args, **optional_args):
+    """
+    get,post,delete,put接口参数验证,验证后的参数以requestdata字典保存
+    :param required_args: 必填参数
+    :param optional_args: 可选参数
+    :return:
+    """
+    def decorator(fn, *args, **kwargs):
+        @functools.wraps(fn)
+        async def inner(self, handler, *args, **kwargs):
+            query_args = {}
+            handler.requestdata = {}
+            if handler.match_info:
+                for k, v in handler.match_info.items():
+                    query_args[k] = v
+            if handler.query:
+                for k,v in handler.query.items():
+                    query_args[k] = v
+            if handler.has_body:
+                for k,v in (await handler.json()).items():
+                    query_args[k] = v
+            def validate_type(key, key_type):
+                try:
+                    if not callable(key_type):
+                        raise ValueError('data type must be callable')
+                    v = query_args[key]
+                    val = v if v is None else key_type(v)
+                    handler.requestdata[key] = val
+                    return True
+                except:
+                    msg = 'Argument {} must be type of {}'.format(key, key_type.__name__)
+                    raise RequestParamError(msg)
+            if required_args:
+                for required_key_type in required_args:
+                    if required_key_type[0] not in query_args:
+                        msg = 'Miss argument {}'.format(required_key_type[0])
+                        raise RequestParamError(msg)
+                    else:
+                        validate_type(required_key_type[0], required_key_type[1])
+
+            if optional_args:
+                for key, type_default in optional_args.items():
+                    if key not in query_args:
+                        query_args[key] = type_default[1]
+                        handler.requestdata.update({key: type_default[1]})
+                    else:
+                        validate_type(key, type_default[0])
+            resp = await fn(self, handler)
+            return resp
+        return inner
+    return decorator
